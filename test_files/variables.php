3d460e61dc2a09620dded09d233a42fee471c1ed
siramk@github.com <siramk@iitbhilai.ac.in>
2020-05-03 19:01:24 +0530

    modified variable.php

--- test_files/variables.php
+++ test_files/variables.php
@@ -10,10 +10,5 @@ EOT;
 
 $x=$y[$z+$y];
 
-echo <<<'EOT'
-My name is "$name". I am printing some $foo->foo.
-Now, I am printing some {$foo->bar[1]}.
-This should not print a capital 'A': \x41
-EOT;
-
+$var="hello"
 ?>
